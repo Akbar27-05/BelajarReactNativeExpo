4c264b47f98aee9c8792b0c1fd0503f8c99ee090
akbar05 <109807611+Akbar27-05@users.noreply.github.com>
2024-02-27 09:19:54 +0700

    First Deploy

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native';
 
 export default function App() {
+  const [valText, setValText] = useState('');
+
+  const handleInput = (text) => {
+    setValText(text);
+  };
+
+  const handleSave = () => {
+    console.log('Nilai input: ', valText);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Some text</Text>
+      <Text style={styles.text}>{valText}</Text>
+
+      <TextInput
+        style={styles.textInput}
+        placeholder="You can type in me"
+        value={valText}
+        onChangeText={handleInput}
+      />
+      <Button title="Simpan" onPress={handleSave} />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
   },
 });
